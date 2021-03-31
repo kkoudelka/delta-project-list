2d8f61ef4af832ab4208ea9425b80b4cf8c62400
Karel Koudelka <k.koudelka01@gmail.com>
2021-04-01 00:15:33 +0200

    feat: added B19-20 projects

--- src/projects/classes/B19-20.ts
+++ src/projects/classes/B19-20.ts
@@ -4,7 +4,114 @@ const B1920: ITrida = {
   title: "4.B - 2019/2020",
   unique: "B19-20",
   domain: "delta-studenti.cz",
-  projects: [],
+  projects: [
+    {
+      title: "Zhodnocení online marketingové strategie společnosti Dům Barev",
+      authorName: "Martin Bureš",
+      subdomain: "b2016burema",
+    },
+    {
+      title: "Studentská Jízda (Cleach)",
+      authorName: "Martin Ďuriš",
+      subdomain: "b2016durima",
+    },
+    {
+      title: "Žolíky",
+      authorName: "Jirka Falta",
+      subdomain: "b2016faltji",
+    },
+    {
+      title: "Music video k písničce Repete od skupiny BlackDisease",
+      authorName: "Vojta Hečlo",
+      subdomain: "b2016heclvo",
+    },
+    {
+      title:
+        "Marketingová komunikace a komunikační mix pro vlastní IT firmu - IT Servis Jiránek",
+      authorName: "Vojtěch Jiránek",
+      subdomain: "b2016jiravo",
+    },
+    {
+      title: "Marketingová kampaň pro firmu MySmartHouse.cz",
+      authorName: "Daniel Jirásek",
+      subdomain: "b2016jirada",
+    },
+    {
+      title: "Sázava GO",
+      authorName: "Jan Kollert",
+      subdomain: "b2016kollja",
+    },
+    {
+      title: "MyBranch",
+      authorName: "Karel Koudelka",
+      subdomain: "b2016koudka",
+    },
+    {
+      title: "Marketingová strategie firmy U Dvou krkoviček s.r.o.",
+      authorName: "Denisa Lohniská",
+      subdomain: "b2016lohnde",
+    },
+    {
+      title:
+        "Návrh celé marketingové strategie a brandu společnosti Zooflor Holice",
+      authorName: "Robin Mácháček",
+      subdomain: "b2016machro",
+    },
+    {
+      title: "Inteligentní turistický průvodce",
+      authorName: "Martin Rohr",
+      subdomain: "b2016rohrma",
+    },
+    {
+      title: "???",
+      authorName: "Jan Sedlák",
+      subdomain: "b2016sedlja",
+    },
+    {
+      title: "Short - The Rapture",
+      authorName: "Ondřej Soukup",
+      subdomain: "b2016soukon",
+    },
+    {
+      title:
+        "Propagace, marketing a organizace regionálního festivalu dokumentárních filmů o lidských právech JEDEN SVĚT 2019",
+      authorName: "Michal Špitálský",
+      subdomain: "b2016spitmi",
+    },
+    {
+      title: "Cvičebnice pro počítačové systémy a sítě",
+      authorName: "Michal Štěrba",
+      subdomain: "b2016stermi",
+    },
+    {
+      title:
+        "Marketingová strategie pro Chovatelsko-zahrádkářské centrum Holice",
+      authorName: "Denisa Štěrbová",
+      subdomain: "b2016sterde",
+    },
+    {
+      title:
+        "Propagace, marketing a organizace regionálního festivalu dokumentárních filmů o lidských právech JEDEN SVĚT 2019",
+      authorName: "Jakub Tesařík",
+      subdomain: "b2016tesaja",
+    },
+    {
+      title: "Re-design časopisu školy Delta",
+      authorName: "Přemysl Tichý",
+      subdomain: "b2016tichpr",
+    },
+    {
+      title: "Balíček 3D low poly modelů",
+      authorName: "Vojtěch Vázler",
+      subdomain: "b2016tichpr",
+    },
+    {
+      title:
+        "Zhodnocení online marketingové strategie e-shopu OKProdej a návrh nové marketingové strategie",
+      authorName: "Filip Zahálka",
+      subdomain: "b2016zahafi",
+    },
+  ],
 };
 
 export default B1920;
