99e644b9cfaca4a8581557f1b13632968874c908
czM1K3 <misamadera1@gmail.com>
2022-02-10 22:21:16 +0100

    New classes

--- src/projects/classes.ts
+++ src/projects/classes.ts
@@ -3,7 +3,9 @@ import A2021 from "./classes/20-21/A20-21";
 import B1819 from "./classes/18-19/B18-19";
 import B1920 from "./classes/19-20/B19-20";
 import B2021 from "./classes/20-21/B20-21";
+import A2022 from "./classes/21-22/A21-22";
+import B2022 from "./classes/21-22/B21-22";
 
-const classes: ITrida[] = [B1819, B1920, A2021, B2021];
+const classes: ITrida[] = [B1819, B1920, A2021, B2021, A2022, B2022];
 
 export default classes;
